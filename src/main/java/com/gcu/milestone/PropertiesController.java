db46d7b3476ffc40701dae34e5c33f8f7d758e98
Caleb Wolin <caleb.wolin@gmail.com>
2023-03-26 21:24:49 -0700

    Updated for CRUD. SQL database attached.

--- src/main/java/com/gcu/milestone/PropertiesController.java
+++ src/main/java/com/gcu/milestone/PropertiesController.java
@@ -5,32 +5,84 @@ import model.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import data.PropertiesDataServiceInterface;
 
 
 @Controller
+@RequestMapping("/properties")
 public class PropertiesController 
 {
-    List<PropertyModel> properties = new ArrayList<PropertyModel>();
+    @Autowired
+    private PropertiesDataServiceInterface service;
+
+    //List<PropertyModel> properties = new ArrayList<PropertyModel>();
 
     public PropertiesController()
     {
-        properties.add(new PropertyModel());
-        properties.add(new PropertyModel());
-        properties.add(new PropertyModel());
-        properties.add(new PropertyModel());
-        properties.add(new PropertyModel());
-        properties.add(new PropertyModel());
-        properties.add(new PropertyModel());
+        //properties = dataService.getOrders();
     }
 
-    @GetMapping("/properties")
-    public String display(Model model)
+    @GetMapping("/")
+    public String showAllOrders(Model model)
     {
-        model.addAttribute("properties", properties);
+        List<PropertyModel> properties = service.getOrders();
+		
+		model.addAttribute("title", "Show all orders");
+		//model.addAttribute("searchOrdersModel", new SearchOrdersModel());
+		model.addAttribute("properties", properties); 
+		return "properties/properties1";
+    }
+
+    @GetMapping("/searchForm")
+    public String displaySearchForm(Model model)
+    {
+        model.addAttribute("title", "Search for Orders");
+        model.addAttribute("searchPropertyModel", new SearchPropertyModel());
+        return "properties/propertiesSearchForm";
+    }
+
+    @PostMapping("/searchResults")
+    public String showProperties(SearchPropertyModel searchModel, Model model)
+    {
+        List<PropertyModel> properties = service.searchOrders(searchModel.getSearchTerm());
+        model.addAttribute("title", "Search for Orders");
+		model.addAttribute("searchPropertyModel", searchModel);
+		model.addAttribute("properties", properties);
         return "properties/properties1";
     }
+
+    @GetMapping("/addNewForm")
+    public String displayAddNewForm(Model model)
+    {
+        model.addAttribute("title", "Add new order");
+		model.addAttribute("propertyModel", new PropertyModel());
+		return "properties/addNewForm";
+
+    }
+
+    @PostMapping("/addNew") 
+	// process a request from the AddOrderForm.  Add a new order to the database.  Show all orders.
+	public String addProperty(PropertyModel property, Model model) {
+		// add the new order
+		service.addOne(property);
+		// get updated list of all the orders
+		List<PropertyModel> properties = service.getOrders(); 
+		
+		// display all orders
+		model.addAttribute("properties", properties); 
+		model.addAttribute("title", "Added an item");
+		model.addAttribute("searchPropertyModel", new SearchPropertyModel()); 
+		return "properties/properties1";
+	}
+
+
+
 
 }
